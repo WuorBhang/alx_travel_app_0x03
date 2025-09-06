911d388ad431b6a496b32c4ca75fbb62c3215d6d
WuorBhang <uhuribhang211@gmail.com>
2025-09-06 04:03:47 +0300

    fixing the allow host domain

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-kabwu+*kxqu6e7g6^wo36
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('alx-travel-app-0x03-24zk.onrender.com', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'alx-travel-app-0x03-24zk.onrender.com']
 
 # Application definition
 
